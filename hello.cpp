fba830b23d83f5b25d4200a9854144be71b602d4
jkunal14 <43882559+jkunal14@users.noreply.github.com>
2018-10-16 02:57:14 +0530

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -4,5 +4,6 @@ int main()
 {
 	cout<<"Hello World, This is my first program uploading using terminal.";
 	cout<<"This is first change :P ";
+	cout<<"Merge krlio xD...bade time baad chlaya git"<<endl;
 	return 0;
 }
